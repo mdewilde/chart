e995023677ae9c2df20ca4d901955599bc122caf
Marceau Dewilde <m@ceau.be>
2016-09-07 20:57:52 +0200

    Prep 2.0.0 release

--- src/main/java/be/ceau/chart/enums/package-info.java
+++ src/main/java/be/ceau/chart/enums/package-info.java
@@ -13,4 +13,7 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 */
+/**
+ * Enums used in data and options objects.
+ */
 package be.ceau.chart.enums;
