98ff98a366655e3195578bc11599510287251895
Marceau Dewilde <m@ceau.be>
2016-09-13 21:10:35 +0200

    Properly annotate for JSON serialization

--- src/main/java/be/ceau/chart/options/scales/XAxis.java
+++ src/main/java/be/ceau/chart/options/scales/XAxis.java
@@ -17,6 +17,13 @@ package be.ceau.chart.options.scales;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_EMPTY)
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class XAxis {
 
 	/**
--- src/main/java/be/ceau/chart/options/scales/YAxis.java
+++ src/main/java/be/ceau/chart/options/scales/YAxis.java
@@ -15,6 +15,13 @@
 */
 package be.ceau.chart.options.scales;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_EMPTY)
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class YAxis {
 
 	/**
