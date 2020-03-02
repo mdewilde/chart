80bbc675bb537062cdc3165cdf302cb358e8f2f8
Marceau Dewilde <m@ceau.be>
2020-03-02 21:24:37 +0100

    Dependency updates

--- src/main/java/be/ceau/chart/options/elements/Fill.java
+++ src/main/java/be/ceau/chart/options/elements/Fill.java
@@ -1,7 +1,24 @@
+/*
+	Copyright 2020 Marceau Dewilde <m@ceau.be>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 package be.ceau.chart.options.elements;
 
-import be.ceau.chart.enums.FillMode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonValue;
+
+import be.ceau.chart.enums.FillMode;
 
 /**
  * <p>
@@ -9,35 +26,53 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * </p>
  */
 public class Fill<T> {
-    @JsonValue
-    private T fill;
 
-    /**
-     * @param fill - If true, fill the area under the line
-     */
-    public Fill(Boolean fill) {
-        this.fill = (T) fill;
-    }
+	@JsonValue
+	private T fill;
 
-    /**
-     * @param index - Absolute dataset index
-     */
-    public Fill(String index) {
-        this.fill = (T) index;
-    }
+	/**
+	 * Default constructor
+	 */
+	public Fill() {
+	}
 
-    /**
-     *
-     * @param index - Relative dataset index
-     */
-    public Fill(Integer index) {
-        this.fill = (T) index;
-    }
+	/**
+	 * @param fill - If true, fill the area under the line
+	 */
+	public Fill(Boolean fill) {
+		this.fill = (T) fill;
+	}
 
-    /**
-     * @param boundary
-     */
-    public Fill(FillMode boundary) {
-        this.fill = (T) boundary;
-    }
+	/**
+	 * @param index - Absolute dataset index
+	 */
+	public Fill(String index) {
+		this.fill = (T) index;
+	}
+
+	/**
+	 *
+	 * @param index - Relative dataset index
+	 */
+	public Fill(Integer index) {
+		this.fill = (T) index;
+	}
+
+	/**
+	 * @param boundary
+	 */
+	public Fill(FillMode boundary) {
+		this.fill = (T) boundary;
+	}
+	
+	@JsonIgnore
+	public T getFill() {
+		return fill;
+	}
+	
+	@JsonIgnore
+	public void setFill(T fill) {
+		this.fill = fill;
+	}
+	
 }
